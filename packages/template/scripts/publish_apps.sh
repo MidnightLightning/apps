f4c8f64cf7e00d3826487a6b01fb576710c04b8c
Carl Larson <cslarson@gmail.com>
2019-04-03 16:16:56 -0400

    removed redundant package.json files causing trouble

--- packages/template/scripts/publish_apps.sh
+++ packages/template/scripts/publish_apps.sh
@@ -18,7 +18,7 @@ cd ~/Projects/daonuts/apps/packages/hamburger
 aragon apm publish major --environment $ENV
 
 cd ~/Projects/daonuts/apps/packages/karma-cap-voting
-aragon apm publish major --environment $ENV
+aragon apm publish major --environment $ENV --debug
 
 cd ~/Projects/daonuts/apps/packages/registry
 aragon apm publish major --environment $ENV
