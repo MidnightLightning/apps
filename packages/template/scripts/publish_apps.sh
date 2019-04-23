164fa6e6ec4d6059ceb9e081f8e45b63ffd00239
Carl Larson <cslarson@gmail.com>
2019-04-23 16:40:21 -0400

    use localhost for REACT_APP_DEFAULT_ETH_NODE and REACT_APP_IPFS_GATEWAY

--- packages/template/scripts/publish_apps.sh
+++ packages/template/scripts/publish_apps.sh
@@ -2,42 +2,46 @@
 case "$1" in
         staging)
             ENV="staging"
+            APM_IPFS_RPC="http://localhost:5001"
             echo environment: $ENV
             ;;
         production)
             ENV="production"
+            APM_IPFS_RPC="http://localhost:5001"
             echo environment: $ENV
             ;;
         docker)
             ENV="docker"
+            APM_IPFS_RPC="http://ipfs:5001"
             echo environment: $ENV
             ;;
         *)
             ENV="default"
+            APM_IPFS_RPC="http://localhost:5001"
             echo environment: $ENV
             ;;
 esac
 
 pushd ../distribution
-aragon apm publish major --environment $ENV
+aragon apm publish major --environment $ENV --ipfs-check false --apm.ipfs.rpc $APM_IPFS_RPC
 popd
 
 pushd ../hamburger
-aragon apm publish major --environment $ENV
+aragon apm publish major --environment $ENV --ipfs-check false --apm.ipfs.rpc $APM_IPFS_RPC
 popd
 
 pushd ../karma-cap-voting
-aragon apm publish major --environment $ENV
+aragon apm publish major --environment $ENV --ipfs-check false --apm.ipfs.rpc $APM_IPFS_RPC
 popd
 
 pushd ../registry
-aragon apm publish major --environment $ENV
+aragon apm publish major --environment $ENV --ipfs-check false --apm.ipfs.rpc $APM_IPFS_RPC
 popd
 
 pushd ../tipping
-aragon apm publish major --environment $ENV
+aragon apm publish major --environment $ENV --ipfs-check false --apm.ipfs.rpc $APM_IPFS_RPC
 popd
 
 pushd ../token-manager
-aragon apm publish major --environment $ENV
+aragon apm publish major --environment $ENV --ipfs-check false --apm.ipfs.rpc $APM_IPFS_RPC
 popd
